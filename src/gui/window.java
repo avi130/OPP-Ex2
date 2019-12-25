bb230af03203f00b230a5744b753d1b31ee88ca6
avi130 <57074327+avi130@users.noreply.github.com>
2019-12-25 21:47:41 +0200

    Add files via upload

--- src/gui/window.java
+++ src/gui/window.java
@@ -15,6 +15,7 @@ import java.io.FileOutputStream;
 import java.io.OutputStreamWriter;
 import java.util.Collection;
 import java.util.LinkedList;
+import java.util.List;
 
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
@@ -35,141 +36,143 @@ import utils.Point3D;
 
 public class window extends JFrame implements ActionListener, MouseListener {
 
-	
+
 	MenuItem Node;
 	//JTextField tf;
 	JButton load;
 	JButton save;
-   
-   // JFileChooser jfc;
+
+	// JFileChooser jfc;
 	int key;
 	DGraph graph;
-	
+
 	Point3D p;
 	graph graph2;
-	
-	
+
+
 	public window(graph g) {
 		this.graph2=g;
 		initGUI();
 	}
-	
 
-	
+
+
 	public  window() {
 		// TODO Auto-generated constructor stub
 		initGUI();
 	}
-	
+
 	private void initGUI() 
 	{
 		this.setSize(1000, 1000);
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
+
 		MenuBar menuBar = new MenuBar();
 		Menu menu = new Menu("Menu");
 		menuBar.add(menu);
 		this.setMenuBar(menuBar);
-		
+
 		MenuItem item1 = new MenuItem("load");
 		item1.addActionListener(this);
 		load= new JButton("load");
-		
+
 		MenuItem item2 = new MenuItem("save");
 		item2.addActionListener(this);
 		save = new JButton("save");
-		
-		
-		
+
+
+
 		menu.add(item1);
 		menu.add(item2);
-		
-		
-		
+
+
+
 		Menu menu2 = new Menu("Algo");
 		menuBar.add(menu2);
 		this.setMenuBar(menuBar);
-	
-		
-		
-		
+
+
+
+
 		Menu menu3 = new Menu("Tests");
 		menuBar.add(menu3);
 		this.setMenuBar(menuBar);
-		
-		
-		
-		
+
+
+
+
 		this.addMouseListener(this);
 	}
-	
-	
-	
+
+
+
 	public void paint(Graphics g)
 	{
 		super.paint(g);
-		
-		
+
+
 		for (node_data p : graph2.getV() ) 
 		{
 			g.setColor(Color.BLUE);
 			Point3D srcPoint = p.getLocation();
 			g.fillOval((int)srcPoint.x(), (int)srcPoint.y(), 10, 10);
-			
-			
-			for(edge_data e: graph2.getE(p.getKey())) {
-				
-				g.setColor(Color.RED);
-				
-				Point3D destPoint = graph2.getNode(e.getDest()).getLocation();
-				g.drawLine((int)srcPoint.x(), (int)srcPoint.y(), (int)destPoint.x(), (int)destPoint.y());
-				
-				g.fillOval((int)destPoint.x(), (int)destPoint.y(), 10, 10);
-				g.drawString(""+e.getWeight(), (int)((srcPoint.x()+destPoint.x())/2),(int)((srcPoint.y()+destPoint.y())/2));														
-			
-				g.setColor(Color.yellow);
-				g.fillOval((int)((0.9*srcPoint.x()+0.1*destPoint.x())), (int)((0.9*srcPoint.y()+0.1*destPoint.y())), 10, 10);
-				
+			g.drawString(""+p.getKey(), (int)srcPoint.x() ,(int)srcPoint.y()-10);														
+
+			if(graph2.getE(p.getKey())!=null) {
+				for(edge_data e: graph2.getE(p.getKey())) {
+
+					g.setColor(Color.RED);
+
+					Point3D destPoint = graph2.getNode(e.getDest()).getLocation();
+					g.drawLine((int)srcPoint.x(), (int)srcPoint.y(), (int)destPoint.x(), (int)destPoint.y());
+
+					//g.fillOval((int)destPoint.x(), (int)destPoint.y(), 10, 10);
+					g.drawString(""+e.getWeight(),(int)((0.1*srcPoint.x()+0.9*destPoint.x()+10)), (int)((0.1*srcPoint.y()+0.9*destPoint.y()+20)));														
+
+					g.setColor(Color.yellow);
+					g.fillOval((int)((0.1*srcPoint.x()+0.9*destPoint.x())), (int)((0.1*srcPoint.y()+0.9*destPoint.y())), 10, 10);
+					g.setColor(Color.BLUE);
+					g.drawString(""+e.getDest() , (int)destPoint.x(), (int)destPoint.y()-10);
+				}
 			}
-			
 		}
 	}
-	
+
 
 	private void save()
 	{
-		 JFileChooser fileChooser = new JFileChooser();
-		 Graph_Algo ga = new Graph_Algo();
-			ga.init(graph2);
-		    int retval = fileChooser.showSaveDialog(save);
-		    if (retval == JFileChooser.APPROVE_OPTION) {
-		    	try {
-					ga.save(fileChooser.getSelectedFile()+".txt");
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-		    }
-		
+		JFileChooser fileChooser = new JFileChooser();
+		Graph_Algo ga = new Graph_Algo();
+		ga.init(graph2);
+		int retval = fileChooser.showSaveDialog(save);
+		if (retval == JFileChooser.APPROVE_OPTION) {
+			try {
+				ga.save(fileChooser.getSelectedFile()+".txt");
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
 	}
-	
-	
+
+
 	private void load()
 	{
-		 JFileChooser fileChooser = new JFileChooser();
-		 Graph_Algo ga = new Graph_Algo();
-		    int retval = fileChooser.showSaveDialog(load);
-		    if (retval == JFileChooser.APPROVE_OPTION) {
-		    	try {
-		    		File selectedFile = fileChooser.getSelectedFile();
-					ga.init(selectedFile.getAbsolutePath());
-					this.graph2 = ga.copy();
-					repaint();
-				} catch (Exception ex) {
-					ex.printStackTrace();
-				}
-		    }
-		    
-		   
+		JFileChooser fileChooser = new JFileChooser();
+		Graph_Algo ga = new Graph_Algo();
+		int retval = fileChooser.showSaveDialog(load);
+		if (retval == JFileChooser.APPROVE_OPTION) {
+			try {
+				File selectedFile = fileChooser.getSelectedFile();
+				ga.init(selectedFile.getAbsolutePath());
+				this.graph2 = ga.copy();
+				repaint();
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+
+
 		/*
 		    Graph_Algo ga = new Graph_Algo();
 			JFileChooser jf = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
@@ -180,47 +183,47 @@ public class window extends JFrame implements ActionListener, MouseListener {
 				this.graph2 = ga.copy();
 				repaint();
 			}
-		    
-		*/
+
+		 */
 	}
-	
-	
+
+
 	@Override
 	public void mouseClicked(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseEntered(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseExited(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
 		// TODO Auto-generated method stub
-	
+
 		repaint();
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent arg0) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		// TODO Auto-generated method stub
 		String str = e.getActionCommand();
-		
+
 		if(str.equals("save"))
 		{
 			save();
@@ -229,44 +232,113 @@ public class window extends JFrame implements ActionListener, MouseListener {
 		{
 			load();
 		}
-		
+
 	}
 
-	
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		graph x= new DGraph();
-		
+		Graph_Algo x= new Graph_Algo();
+		graph xx= new DGraph();
+/*
 		node_data a= new node(1,new Point3D(800,705,10),3);
 		node_data b= new node(2,new Point3D(600,350,90),12);
 		node_data c= new node(3,new Point3D(300,250,80),7);
 		node_data d= new node(4,new Point3D(900,550,60),6);
+		node_data e= new node(5,new Point3D(150,550,60),6);
+		node_data aa= new node(6,new Point3D(190,705,10),3);
+		node_data ba= new node(7,new Point3D(100,350,90),12);
+		node_data ca= new node(8,new Point3D(97,123,80),7);
+		node_data da= new node(9,new Point3D(300,550,60),6);
+		node_data ea= new node(10,new Point3D(345,512,60),6);
+
+
+
+		xx.addNode(a);
+		xx.addNode(b);
+		xx.addNode(c);
+		xx.addNode(d);
+		xx.addNode(e);
+		xx.addNode(aa);
+		xx.addNode(ba);
+		xx.addNode(ca);
+		xx.addNode(da);
+		xx.addNode(ea);
+
+		xx.connect(1, 2, 1);
+		xx.connect(1, 3, 50);
+		xx.connect(1, 4, 50);
+		xx.connect(2, 3, 1);
+		xx.connect(4, 3, 10);
+		xx.connect(3, 2, 10);
+		xx.connect(2, 1, 10);
+		xx.connect(2, 4, 10);
+		xx.connect(4, 1, 10);
+		xx.connect(5, 2, 3);
+		xx.connect(2, 5, 8);
+		xx.connect(1, 5,9);
+
+		xx.connect(9, 1, 10);
+		xx.connect(4, 7, 10);
+		xx.connect(8, 2, 10);
+		xx.connect(2, 9, 10);
+		xx.connect(4, 7, 10);
+		xx.connect(7, 6, 10);
+		xx.connect(5, 1, 10);
+		xx.connect(2, 8, 2);
+		xx.connect(8, 5, 2);
+
+		x.init(xx);
+*/
 		
-		edge_data aa=new edge(200, 400, 10);
-		edge_data ab=new edge(100, 190, 60);
-		edge_data ac=new edge(500, 800, 20);
-		edge_data ad=new edge(400, 720, 70);
-		
-		x.addNode(a);
-		x.addNode(b);
-		x.addNode(c);
-		x.addNode(d);
-		x.connect(1, 2, 5);
-		x.connect(1, 3, 5);
-		x.connect(1, 4, 5);
-		x.connect(2, 3, 10);
-		x.connect(4, 3, 10);
-		x.connect(3, 2, 10);
-		x.connect(2, 1, 10);
-		x.connect(2, 4, 10);
-		x.connect(4, 1, 10);
 		
 		
 		
 		
 		
-		window GUI = new window();
+		node_data ba= new node(1,new Point3D(700,350,90),12);
+		node_data ca= new node(2,new Point3D(97,123,80),7);
+		node_data da= new node(3,new Point3D(300,750,60),6);
+		node_data ea= new node(4,new Point3D(345,512,60),6);
+		
+		
+		xx.addNode(ba);
+		xx.addNode(ca);
+		xx.addNode(da);
+		xx.addNode(ea);
+		
+		
+		//xx.connect(1, 2, 5);
+	//	xx.connect(1, 3, 50);
+	//	xx.connect(1, 4, 50);
+	//	xx.connect(2, 1, 1);
+		xx.connect(2, 3, 10);
+		//xx.connect(2, 4, 50);
+		xx.connect(3, 1, 50);
+		xx.connect(3, 2, 1);
+		xx.connect(3, 4, 10);
+		xx.connect(4, 1, 50);
+		xx.connect(4, 2, 50);
+		xx.connect(4, 3, 1);
+		
+		
+		x.init(xx);
+		
+	//	System.out.println(x.shortestPathDist(1,2));
+		
+		List<node_data> q=x.shortestPath(1, 2);
+	
+		for(int i=0;i<q.size();i++){
+		System.out.print(q.get(i).getKey()+" ,");		} 
+	//	System.out.println(x.isConnected());
+		
+		
+		window GUI = new window(xx);
 		GUI.setVisible(true);
 	}
+
+
+
+
 
 }
